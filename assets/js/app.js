22b594815b56b065f41b4b8ff54ac2aad3a2c86c
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-10-25 17:28:13 +0500

    minor fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,37 @@
+import { handleSubmit } from "./handleSubmit.js";
 import { maskPhone } from "./maskPhone.js";
 import { validateInputs } from "./validateInputs.js";
 
-const regexPhone = /^\+7 \(\d{3}\) \d{3} \d{4}$/;
-const regexMail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$/;
-const nameRegex = /^[A-Za-zА-Яа-яЁё\- ]+$/;
-const textRegex = /^[A-Za-z0-9А-Яа-яЁё ]+$/;
-const form = document.querySelector("#form1");
+const REGEXPHONE = /^\+7 \(\d{3}\) \d{3} \d{4}$/;
+const REGEXMAIL = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$/;
+const REGEXNAME = /^[A-Za-zА-Яа-яЁё\- ]+$/;
+const REGEXTEXT = /^[A-Za-z0-9А-Яа-яЁё ]+$/;
+
+validateInputs(".feedback-form__input--phone", REGEXPHONE);
+validateInputs(".feedback-form__input--email", REGEXMAIL);
+validateInputs(".feedback-form__input--name", REGEXNAME);
+validateInputs(".feedback-form__textarea", REGEXTEXT);
 
 maskPhone(".feedback-form__input--phone");
 
-validateInputs(".feedback-form__input--phone", regexPhone);
-validateInputs(".feedback-form__input--email", regexMail);
-validateInputs(".feedback-form__input--name", nameRegex);
-validateInputs(".feedback-form__textarea", textRegex);
+const form = document.querySelector("#form1");
+const LINKFORSUBMITDATA = "https://jsonplaceholder.typicode.com/posts";
 
 form.addEventListener("submit", event => {
   event.preventDefault();
+
+  const button = form.querySelector(".feedback-form__button");
+  const checkbox = form.querySelector(".feedback-form__checkbox");
+  if (!checkbox.checked) {
+    return;
+  }
+
+  const fields = form.querySelectorAll(".feedback-form__field");
+  for (const field of fields) {
+    if (field.classList.contains("error")) {
+      return;
+    }
+  }
 
   const formData = new FormData(form);
   for (const value of formData.values()) {
@@ -24,11 +40,8 @@ form.addEventListener("submit", event => {
     }
   }
 
-  const checkbox = form.querySelector(".feedback-form__checkbox");
-  if (!checkbox.checked) {
-    return;
-  }
+  button.classList.add("js-loading");
+  handleSubmit(LINKFORSUBMITDATA, formData, button, "js-loading");
 
-  console.log("submit");
   form.reset();
 });
